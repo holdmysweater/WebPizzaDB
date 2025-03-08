c84ade79fc0bdad57fe64edd385f68b752b8613e
Elizaveta Lim <eliza.volga21@gmail.com>
2025-03-09 00:51:19 +0300

    feat: title and logo

--- pizzeria.php
+++ pizzeria.php
@@ -5,7 +5,8 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-    <title>Website</title>
+    <title>Пиццерия</title>
+    <link rel="icon" type="image/png" sizes="32x32" href="./assets/images/logo.svg">
     <link rel="stylesheet" href="./assets/css/bootstrap.css"/>
     <link rel="stylesheet" href="./assets/css/style.css"/>
 </head>
